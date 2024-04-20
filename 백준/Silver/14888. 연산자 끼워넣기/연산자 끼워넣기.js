b4bb25835f1444c8963387118a912aaef7c0501b
이나경 <66313756+nakyoung98@users.noreply.github.com>
2024-04-21 00:55:22 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 228 ms, Memory: 12384 KB -BaekjoonHub

---
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.js
@@ -0,0 +1,59 @@
+  const fs = require("fs");
+  const rawInput = fs.readFileSync('dev/stdin').toString().split(/\n/g);
+  const N = +rawInput.shift();
+  const A = rawInput.shift().split(/ /g).map(Number);
+  const op = rawInput.shift().split(/ /g).map(Number);
+
+  let min = Infinity;
+  let max = -Infinity;
+
+  const recurse = (n, now, ops) => {
+    // 종료부
+    if (N === n) {
+      // 최소보다 작으면 최소를 변경
+      if (min > now) {
+        min = now;
+      }
+
+      // 최대보다 크면 최대를 변경
+      if (max < now) {
+        max = now;
+      }
+
+      return;
+    }
+
+    //실행부
+
+    if (ops[0] > 0) {
+      const newOp = ops.slice();
+      newOp[0] -= 1;
+      recurse(n + 1, now + A[n], newOp);
+    }
+    if (ops[1] > 0) {
+      const newOp = ops.slice();
+      newOp[1] -= 1;
+      recurse(n + 1, now - A[n], newOp);
+    }
+    if (ops[2] > 0) {
+      const newOp = ops.slice();
+      newOp[2] -= 1;
+      recurse(n + 1, now * A[n], newOp);
+    }
+    if (ops[3] > 0) {
+      const newOp = ops.slice();
+      newOp[3] -= 1;
+      if (now < 0 && A[n] < 0) {
+        recurse(n + 1, Math.trunc(-now / -A[n]), newOp);
+      } else if (now < 0 || A[n] < 0) {
+        recurse(n + 1, -Math.trunc(Math.abs(now) / Math.abs(A[n])), newOp);
+      } else {
+        recurse(n + 1, Math.trunc(now / A[n]), newOp);
+      }
+    }
+  };
+
+  recurse(1, A[0], op);
+
+  console.log(max === 0 ? 0 : max);
+  console.log(min === 0 ? 0 : min);
