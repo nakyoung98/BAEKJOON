780c12f79448db5ee14217f43c02ce0761a6e344
이나경 <66313756+nakyoung98@users.noreply.github.com>
2024-04-21 13:39:46 +0900

    [Silver I] Title: 부등호, Time: 220 ms, Memory: 11856 KB -BaekjoonHub

---
+++ 백준/Silver/2529. 부등호/부등호.js
@@ -0,0 +1,52 @@
+  const fs = require("fs");
+  const rawInput = fs.readFileSync('dev/stdin').toString().split(/\n/g);
+  const K = +rawInput.shift();
+  const Arrows = rawInput.shift().split(/ /g);
+  const Numbers = new Array(K + 1);
+
+  let min = Infinity;
+  let max = -Infinity;
+
+  const recurse = (k) => {
+    // 조기 리턴 (중복값 검사)
+    if (Numbers.indexOf(Numbers[k]) !== k) return;
+
+    // 끝까지 도달 시 종료
+    if (k === K) {
+      const answer = Numbers.join("");
+      if (+answer > +max) {
+        max = answer;
+      }
+      if (+answer < +min) {
+        min = answer;
+      }
+
+      return;
+    }
+
+    for (let n = 0; n < 10; n++) {
+      switch (Arrows[k]) {
+        case "<":
+          if (Numbers[k] < n) {
+            Numbers[k + 1] = n;
+            recurse(k + 1);
+          }
+          break;
+        case ">":
+          if (Numbers[k] > n) {
+            Numbers[k + 1] = n;
+            recurse(k + 1);
+          }
+          break;
+      }
+    }
+  };
+
+  // 실행부
+  for (let n = 0; n < 10; n++) {
+    Numbers[0] = n;
+    recurse(0);
+  }
+
+  console.log(max);
+  console.log(min);
