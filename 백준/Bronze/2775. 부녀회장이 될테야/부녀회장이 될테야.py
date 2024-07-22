55ece6e1269cbab30c13a85946afb13338ca653f
이나경 <66313756+nakyoung98@users.noreply.github.com>
2024-07-22 19:47:50 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -1,35 +1,24 @@
 import sys
 
+
+def solution(T):
+    result = []
+    for _ in range(T):
+        floor = int(sys.stdin.readline())
+        room = int(sys.stdin.readline())
+
+        #  기본 값 0층 dp[0] 과 나머지 층에 대한 빈 배열 생성 : 이중배열
+        ## [*][0]  = 0 으로 유지
+        dp = [[i for i in range(room + 1)]] + [[0] * (room + 1) for _ in range(floor)]
+
+        for nowFloor in range(1, floor + 1):
+            for nowRoom in range(1, room + 1):
+                dp[nowFloor][nowRoom] = sum(dp[nowFloor - 1][: nowRoom + 1])
+
+        result.append(dp[nowFloor][nowRoom])
+
+    return "\n".join(map(str, result))
+
+
 T = int(sys.stdin.readline())
-
-for _ in range(T):
-    k = int(sys.stdin.readline())
-    n = int(sys.stdin.readline())
-
-    #dp를 위한 임시 저장 리스트
-    aparts = [[0 for _ in range(n+1)] for _ in range(k)]
-    
-    #1층의 기저 값 입력
-    for i in range(n+1):
-        aparts[0][i] = i
-
-    #k-1층 n호까지의 인원수 구하기 
-    for floor in range(1,k):
-
-        for room in range(1,n+1):
-            peopleSum = 0
-            
-            for people in range(room):
-                peopleSum += aparts[floor-1][people]
-            peopleSum += aparts[floor-1][room]
-
-            aparts[floor][room] = peopleSum
-    
-    #k층 n호의 인원수
-        #k-1층의 1호부터 n호까지 합한다.
-    result = 0
-    for room in range(1,n+1):
-        result += aparts[k-1][room]
-
-
-    print(result)
+print(solution(T))
