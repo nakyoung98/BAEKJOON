495e1fc806784bb2fb6b79445e4a343522387d60
이나경 <66313756+nakyoung98@users.noreply.github.com>
2024-07-26 17:12:02 +0900

    [Silver I] Title: 센티와 마법의 뿅망치, Time: 172 ms, Memory: 37044 KB -BaekjoonHub

--- 백준/Silver/19638. 센티와 마법의 뿅망치/센티와 마법의 뿅망치.py
+++ 백준/Silver/19638. 센티와 마법의 뿅망치/센티와 마법의 뿅망치.py
@@ -1,113 +1,61 @@
-class MaxHeap:
-    def __init__(self, array=[]):
-        self.heap = [0] + array
-        self._build_heap()
-
-    def _build_heap(self):
-        for i in range((len(self.heap) - 1) // 2, 0, -1):
-            self._sift_down(i)
-
-    def insert(self, value):
-        self.heap.append(value)
-        self._sift_up(len(self.heap) - 1)
-
-    def popMost(self):
-        if len(self.heap) <= 1:
-            raise IndexError("더이상 꺼낼 요소가 없습니다")
-        if len(self.heap) == 2:
-            return self.heap.pop()
-
-        max_val = self.heap[1]
-        self.heap[1] = self.heap.pop()
-        self._sift_down(1)
-        return max_val
-
-    def _sift_up(self, i):
-        while i > 1 and self.heap[i] > self.heap[i // 2]:
-            self.heap[i], self.heap[i // 2] = self.heap[i // 2], self.heap[i]
-            i //= 2
-
-    def _sift_down(self, i):
-        max_index = i
-        while True:
-            left = 2 * i
-            right = 2 * i + 1
-            if left < len(self.heap) and self.heap[left] > self.heap[max_index]:
-                max_index = left
-            if right < len(self.heap) and self.heap[right] > self.heap[max_index]:
-                max_index = right
-            if i == max_index:
-                break
-            self.heap[i], self.heap[max_index] = self.heap[max_index], self.heap[i]
-            i = max_index
-
-    def __len__(self) -> int:
-        return len(self.heap) - 1
-
-    def __str__(self) -> str:
-        return "[" + ", ".join(map(str, self.heap[1:])) + "]"
-
-
 import sys
+import heapq
 
 
 class Hammer:
-    def __init__(self, maxHammeringCount):
-        self.maxHammeringCount = maxHammeringCount
+    class ExhaustedError(Exception):
+        """해머 사용횟수가 모두 소진되었습니다"""
+
+        pass
+
+    def __init__(self, maxHitCount):
+        self.maxHitCount = maxHitCount
         self.count = 0
 
-    def hit(self, value):
+    def hit(self, data):
         if self.isExhausted():
-            raise ValueError("횟수를 모두 소진했습니다")
+            raise self.ExhaustedError()
 
+        self.maxHitCount -= 1
         self.count += 1
-        self.maxHammeringCount -= 1
-
-        if value == 1:
-            return 1
-
-        return value // 2
+        return data // 2 if data // 2 != 0 else 1
 
     def isExhausted(self):
-        return self.maxHammeringCount == 0
-
-    def __len__(self) -> int:
-        return self.maxHammeringCount
+        return self.maxHitCount <= 0
 
 
-def solution(population, centiH, maxHammerCount):
-    result = "YES"
+def solution(population, centiH, maxHitCount):
     heights = []
+    hammer = Hammer(maxHitCount)
 
-    # 센티보다 큰 거인 키 수집
     for _ in range(population):
         height = int(sys.stdin.readline())
+
+        # 큰 것들만 저장
         if height >= centiH:
-            heights.append(height)
+            heights.append(-height)
 
-    # heigh ts maxHeap 정렬
-    heights = MaxHeap(heights)
-    hammer = Hammer(maxHammerCount)
+    # heapify로 배열 => heap
+    # 최소힙을 만들기 때문에, 최대값을 픽해야하는 현 문제에서는 음수로 저장
+    heapq.heapify(heights)
 
-    # heights에서 하나씩 꺼내어 해머링
-    while not hammer.isExhausted() and heights:
-        try:
-            peek = heights.popMost()
-            peek = hammer.hit(peek)
-            if peek >= centiH:
-                heights.insert(peek)
-        except IndexError:
-            break
-        except ValueError:
-            break
+    # maxHitCount나 heights의 크기를 비교해가며 pop
+    while not hammer.isExhausted() and len(heights) > 0:
+        # 거인 뽑아서
+        peek = -heapq.heappop(heights)
+        # 키작게하고
+        peek = hammer.hit(peek)
+        # 크면 다시 heap에 넣고
+        if peek >= centiH:
+            heapq.heappush(heights, -peek)
 
-    if not heights:
+    if heights:
+        print("NO")
+        print(-heights[0])
+    else:
         print("YES")
         print(hammer.count)
-    else:
-        print("NO")
-        print(heights.popMost())
 
 
-population, centiH, maxHammerCount = map(int, sys.stdin.readline().split())
-solution(population, centiH, maxHammerCount)
+population, centiH, maxHitCount = map(int, sys.stdin.readline().split())
+solution(population, centiH, maxHitCount)
