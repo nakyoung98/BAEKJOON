e71c1f7f2ed2a1f9a682bbd4447b252371eaec19
이나경 <66313756+nakyoung98@users.noreply.github.com>
2024-07-29 13:50:19 +0900

    [Silver IV] Title: 30번, Time: 176 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/13116. 30번/30번.py
@@ -0,0 +1,50 @@
+import sys
+
+input = sys.stdin.readline
+
+T = int(input())
+
+
+def code(T):
+    for _ in range(T):
+        A, B = map(int, input().split())
+        result = solution(A, B)
+
+        print(10 * result)
+
+
+def solution(A, B):
+    AandParents = getHeapParents(A)
+    BandParents = getHeapParents(B)
+
+    i = -1
+    mostParent = 1
+    while i >= -len(AandParents) and i >= -len(BandParents):
+
+        # 조상이 분기되면 종료
+        if AandParents[i] != BandParents[i]:
+            break
+
+        # 공통조상 업데이트
+        mostParent = AandParents[i]
+
+        i -= 1
+
+    return mostParent
+
+
+def getHeapParents(N):
+    NandParents = []
+
+    while True:
+        NandParents.append(N)
+
+        if N == 1:
+            break
+
+        N = N // 2
+
+    return NandParents
+
+
+code(T)
