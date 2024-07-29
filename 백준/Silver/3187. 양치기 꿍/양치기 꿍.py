b010134e58fd81749f120309661061faca66a928
이나경 <66313756+nakyoung98@users.noreply.github.com>
2024-07-30 01:06:22 +0900

    [Silver I] Title: 양치기 꿍, Time: 140 ms, Memory: 34068 KB -BaekjoonHub

--- 백준/Silver/3187. 양치기 꿍/양치기 꿍.py
+++ 백준/Silver/3187. 양치기 꿍/양치기 꿍.py
@@ -1,59 +1,58 @@
 import sys
+from collections import deque
 
-sys.setrecursionlimit(10**5)
 input = sys.stdin.readline
 
 
-R, C = map(int, input().split())
+def bfs(y, x, rCount, cCount, room):
+    sheep, wolf = 0, 0
+    queue = deque([(y, x)])
+
+    # 울타리 하나 bfs 탐색
+    while queue:
+        y, x = queue.popleft()
+
+        # 현위치가 바운더리 밖 무시
+        if y < 0 or y >= rCount or x < 0 or x >= cCount:
+            continue
+
+        state = room[y][x]
+
+        # 울타리면 건너뛰기
+        if state == "#":
+            continue
+
+        # 양 수 세기
+        if state == "v":
+            wolf += 1
+        if state == "k":
+            sheep += 1
+
+        # 방문한 곳을 다시 방문할 수 없도록 #처리
+        room[y][x] = "#"
+
+        d = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        for dy, dx in d:
+            queue.append((y + dy, x + dx))
+
+    return sheep if sheep > wolf else -wolf
 
 
-def solution(R, C):
-    places = [list(input().rstrip()) for _ in range(R)]
-    visited = [[0 for _ in range(C)] for _ in range(R)]
-
-    result = {"sheep": 0, "wolf": 0}
+def solution():
+    R, C = map(int, input().split())
+    room = [list(input().rstrip()) for _ in range(R)]
+    sheep = 0
+    wolf = 0
 
     for r in range(R):
         for c in range(C):
-            sheep, wolf = visit(r, c, R, C, visited, places)
-            if sheep > wolf:
-                result["sheep"] += sheep
+            result = bfs(r, c, R, C, room)
+            if result > 0:
+                sheep += result
             else:
-                result["wolf"] += wolf
+                wolf -= result
 
-    return result["sheep"], result["wolf"]
-
-
-def visit(r, c, R, C, visited, places):
-    # 범위를 벗어나면 돌아가기
-    if r < 0 or r >= R or c < 0 or c >= C:
-        return 0, 0
-
-    # 방문했던 곳이면 돌아가기
-    if visited[r][c] == 1:
-        return 0, 0
-
-    # 울타리면 탐색 종료
-    if places[r][c] == "#":
-        return 0, 0
-
-    # 방문기록
-    visited[r][c] = 1
-
-    sheep, wolf = (
-        (1, 0) if places[r][c] == "k" else (0, 1) if places[r][c] == "v" else (0, 0)
-    )
-
-    # 전방위 탐색
-    d = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-    for dx, dy in d:
-        s, w = visit(r + dx, c + dy, R, C, visited, places)
-        sheep += s
-        wolf += w
-
-    # 탐색 끝나면 (주변이 다 1이라는 것이니, 숫자 센 결과 정리)
-    # 양수: 양, 음수: 늑대가 살아남았다는 의미
-    return sheep, wolf
+    print(sheep, wolf)
 
 
-print(*solution(R, C))
+solution()
