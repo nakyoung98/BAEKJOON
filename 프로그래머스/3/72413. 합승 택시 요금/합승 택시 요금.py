15172fbc6ea384bd098d4af583ba0d9f61d2fc7c
이나경 <66313756+nakyoung98@users.noreply.github.com>
2024-10-21 17:08:01 +0900

    [level 3] Title: 합승 택시 요금, Time: 1765.51 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -1,38 +1,53 @@
 import math
-"""
-다익스트라
-"""
+import heapq
 
 def solution(n, s, a, b, fares):
-    start = s
-    a_end = a
-    b_end = b
-    
-    # 요금표만들기 (1~n번까지)
-    routes = [[math.inf] * (n+1) for _ in range(n+1)]
-    for i in range(1,n+1):
-        routes[i][i] = 0
-    
+    routes = [[math.inf if c != r else 0 for c in range(n+1)] for r in range(n+1)]
+
     for fare in fares:
         node1 = fare[0]
         node2 = fare[1]
         fee = fare[2]
         
-        routes[node1][node2] = fee
-        routes[node2][node1] = fee
-        
-    # 전체에서 출발하고 전체 노드에 도착하는 각 요금 구하기
-    for m in range(1,n+1):
-        for s in range(1,n+1):
-            for e in range(1,n+1):
-                if routes[s][m] + routes[m][e] <= routes[s][e]:
-                    routes[s][e] = routes[s][m] + routes[m][e]
-                    
-    # 최소 요금 구하기
-    # 중간 경유지까지의 요금 + m to a + m to b 요금이 최소인 경우 구하기
-    min_fee = routes[start][a_end] + routes[start][b_end] # 초기값으로, 각자 알아서 갔을 때 비용
-    for m in range(1,n+1):
-        if routes[start][m] + routes[m][a_end] + routes[m][b_end] < min_fee:
-            min_fee = routes[start][m] + routes[m][a_end] + routes[m][b_end]
+        routes[node1][node2], routes[node2][node1] = fee, fee
+    
+    s_dijk = dijk(n, s, routes)
+    a_dijk = dijk(n, a, routes)
+    b_dijk = dijk(n, b, routes)
+    
+    # 경유지 기반 값 계산
+    min_fee = a_dijk[s] + b_dijk[s] # 기본값: 각자 따로 갔을 때
+    for via in range(1,n+1):
+        if min_fee > s_dijk[via] + a_dijk[via] + b_dijk[via]:
+              min_fee = s_dijk[via] + a_dijk[via] + b_dijk[via]  
         
     return min_fee
+        
+    
+def dijk(node_count, start, routes):
+    result = [math.inf]*(node_count+1)
+    waits = [(0,start)]
+    
+    while waits:
+        cur_dist, cur_node = heapq.heappop(waits)
+        
+        # 가능 시, 최소값 갱신
+        if result[cur_node] > cur_dist:
+            result[cur_node] = cur_dist
+        
+        # 현재 위치에서 갈 수 있는 거리 얻기
+        for next_node in range(1, node_count+1):
+            if result[next_node] == math.inf:
+                # heapq에 다음 경로까지 거리들 추가하기
+                next_dist = cur_dist + routes[cur_node][next_node]
+                if next_dist != math.inf:
+                    heapq.heappush(waits, (next_dist, next_node))
+
+    return result
+            
+            
+        
+        
+    
+        
+        
