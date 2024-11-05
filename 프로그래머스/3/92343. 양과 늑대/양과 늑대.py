4b5b75f7f0bd17c535df2fa1cb052fc0e3fe9990
이나경 <66313756+nakyoung98@users.noreply.github.com>
2024-11-05 18:35:30 +0900

    [level 3] Title: 양과 늑대, Time: 5.94 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
@@ -2,36 +2,36 @@ from collections import deque
 
 def solution(info, edges):
     # 2차원 배열 트리 17*17이므로 크지 않음
-    tree = [[-1] * len(info) for _ in range(len(info))]
+    tree = [[] for _ in range(len(info))]
+    sheep_count = info.count(0)
     # 0: 양, 1: 늑대
     for edge in edges:
         parent = edge[0]
         child = edge[1]
         
-        tree[parent][child] = info[child]
+        tree[parent].append(child)
         
     max_sheep = 1
     stack = deque([(0, 1, 0,set([0]))]) # index, sheep, wolf, 방문한 모든 자식 노드
     while stack:
         cur_idx, cur_sheep, cur_wolf, possible_nodes = stack.pop()
-        possible_nodes.remove(cur_idx)
+                
+        next_possible_nodes = possible_nodes.copy()
+        next_possible_nodes.remove(cur_idx)
         
-        for i in range(len(info)):
-            child = tree[cur_idx][i] 
-            if child == -1: # 자식관계아니면 나가라
-                continue
-            else:
-                possible_nodes.add(i)
+        next_possible_nodes.update(tree[cur_idx]) 
         
-        for possible_node in possible_nodes:
+        for possible_node in next_possible_nodes:
             node_info = info[possible_node] 
             if node_info == 0: # 양이면
-                stack.append((possible_node, cur_sheep+1, cur_wolf,possible_nodes.copy()))
+                stack.append((possible_node, cur_sheep+1, cur_wolf,next_possible_nodes))
             elif node_info == 1: #늑대면
                 if cur_wolf + 1 < cur_sheep:
-                    stack.append((possible_node, cur_sheep, cur_wolf+1, possible_nodes.copy()))
+                    stack.append((possible_node, cur_sheep, cur_wolf+1, next_possible_nodes))
         if max_sheep < cur_sheep:
             max_sheep = cur_sheep
+        if max_sheep == sheep_count:
+            break
                         
     
     return max_sheep
